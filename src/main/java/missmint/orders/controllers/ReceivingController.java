6129c2c29bb8c4b544c3a6a84c4f51bc45f3b7cd
vilaureu <vilaureu@users.noreply.github.com>
2019-11-07 09:22:30 +0100

    start receiving logic

--- src/main/java/missmint/orders/controllers/ReceivingController.java
+++ src/main/java/missmint/orders/controllers/ReceivingController.java
@@ -1,25 +1,53 @@
 package missmint.orders.controllers;
 
 import missmint.orders.forms.ReceivingForm;
+import missmint.orders.order.MissMintOrder;
 import missmint.orders.services.ServiceRepository;
+import org.salespointframework.order.Order;
+import org.salespointframework.quantity.Quantity;
+import org.salespointframework.time.BusinessTime;
+import org.salespointframework.useraccount.UserAccount;
+import org.salespointframework.useraccount.web.LoggedIn;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.validation.Valid;
+import java.time.LocalDate;
 
 @Controller
 public class ReceivingController {
 	private ServiceRepository serviceRepository;
+	private BusinessTime time;
 
-	public ReceivingController(ServiceRepository serviceRepository) {
+	public ReceivingController(ServiceRepository serviceRepository, BusinessTime time) {
 		this.serviceRepository = serviceRepository;
+		this.time = time;
 	}
 
-	@GetMapping("/receiving")
+	@GetMapping("/orders/receiving")
 	//TODO @PreAuthorize("isAuthenticated()")
-	public String listOrders(Model model, ReceivingForm form) {
+	public String receiving(Model model, ReceivingForm form) {
 		model.addAttribute("form", form);
 		model.addAttribute("services", serviceRepository.findAll());
 		return "receiving";
+	}
+
+	@PostMapping("/orders/receiving")
+	//TODO @PreAuthorize("isAuthenticated()")
+	public String cost(@Valid ReceivingForm receivingForm, Errors errors, Model model , @LoggedIn UserAccount userAccount) {
+		if (errors.hasErrors()) {
+			return receiving(model, receivingForm);
+		}
+
+		Order order = new Order(userAccount);
+		order.addOrderLine(serviceRepository.findById(receivingForm.getService()).orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST)).getProduct(), Quantity.of(1));
+		MissMintOrder missMintOrder = new MissMintOrder(receivingForm.getCustomer(), order, time.getTime().toLocalDate());
+
+		return "cost";
 	}
 }
--- src/main/java/missmint/orders/order/MissMintOrder.java
+++ src/main/java/missmint/orders/order/MissMintOrder.java
@@ -5,21 +5,25 @@ import org.salespointframework.order.Order;
 
 import javax.persistence.*;
 import java.sql.Date;
+import java.time.LocalDate;
 
 @Entity
 public class MissMintOrder {
 	private @Id @GeneratedValue long id;
 	private String customer;
-	private Date inbound;
-	private Date expectedFinished;
-	private Date finished;
-
-	@ManyToOne
-	private Service service;
+	private LocalDate inbound;
+	private LocalDate expectedFinished;
+	private LocalDate finished;
 
 	@OneToOne
 	private Order order;
 
 	public MissMintOrder() {
+	}
+
+	public MissMintOrder(String customer, Order order, LocalDate inbound) {
+		this.customer = customer;
+		this.order = order;
+		this.inbound = inbound;
 	}
 }
