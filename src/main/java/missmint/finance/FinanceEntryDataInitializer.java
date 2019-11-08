4da636fc8149f74ddef81ba56ba5106e09eb4772
Anna <angelcat.ua@gmail.com>
2019-11-08 01:15:59 +0100

    Finanz java klassen

---
+++ src/main/java/missmint/finance/FinanceEntryDataInitializer.java
@@ -0,0 +1,14 @@
+package missmint.finance;
+
+import org.salespointframework.core.DataInitializer;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class FinanceEntryDataInitializer implements DataInitializer {
+
+	@Override
+	public void initialize() {
+
+	}
+}
