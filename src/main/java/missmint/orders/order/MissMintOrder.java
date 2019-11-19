c4e67fde9298431a5f59a3234cb2de80943a8049
vilaureu <vilaureu@users.noreply.github.com>
2019-11-19 15:08:22 +0100

    create service initializer

--- src/main/java/missmint/orders/order/MissMintOrder.java
+++ src/main/java/missmint/orders/order/MissMintOrder.java
@@ -4,6 +4,7 @@ import missmint.orders.service.Service;
 import org.salespointframework.order.Order;
 import org.salespointframework.quantity.Quantity;
 import org.salespointframework.useraccount.UserAccount;
+import org.springframework.util.Assert;
 
 import javax.persistence.Entity;
 import java.time.LocalDate;
@@ -22,6 +23,12 @@ public class MissMintOrder extends Order {
 
 	public MissMintOrder(UserAccount userAccount, String customer, LocalDate inbound, LocalDate expectedFinished, Service service) {
 		super(userAccount);
+
+		Assert.notNull(customer, "customer must not be null");
+		Assert.notNull(inbound, "inbound must not be null");
+		Assert.notNull(expectedFinished, "expectedFinished must not be null");
+		Assert.notNull(service, "service must not be null");
+
 		this.customer = customer;
 		this.inbound = inbound;
 		this.expectedFinished = expectedFinished;
@@ -30,51 +37,12 @@ public class MissMintOrder extends Order {
 	}
 
 	public LocalDate getLatestFinished() {
-		Objects.requireNonNull(getExpectedFinished());
-		return getExpectedFinished().isAfter(getFinished()) ? getExpectedFinished() : getFinished();
+		Assert.notNull(finished, "finished must not be null");
+
+		return expectedFinished.isAfter(finished) ? expectedFinished : finished;
 	}
 
 	public boolean canPickUp() {
-		return getOrderState() == OrderState.FINISHED || getOrderState() == OrderState.STORED;
-	}
-
-	public String getCustomer() {
-		return customer;
-	}
-
-	public void setCustomer(String customer) {
-		this.customer = customer;
-	}
-
-	public LocalDate getInbound() {
-		return inbound;
-	}
-
-	public void setInbound(LocalDate inbound) {
-		this.inbound = inbound;
-	}
-
-	public LocalDate getExpectedFinished() {
-		return expectedFinished;
-	}
-
-	public void setExpectedFinished(LocalDate expectedFinished) {
-		this.expectedFinished = expectedFinished;
-	}
-
-	public LocalDate getFinished() {
-		return finished;
-	}
-
-	public void setFinished(LocalDate finished) {
-		this.finished = finished;
-	}
-
-	public OrderState getOrderState() {
-		return orderState;
-	}
-
-	public void setOrderState(OrderState orderState) {
-		this.orderState = orderState;
+		return orderState == OrderState.FINISHED || orderState == OrderState.STORED;
 	}
 }
---
+++ src/main/java/missmint/orders/service/ServiceDataInitializer.java
@@ -0,0 +1,59 @@
+package missmint.orders.service;
+
+import com.mysema.commons.lang.Assert;
+import org.javamoney.moneta.Money;
+import org.salespointframework.catalog.Catalog;
+import org.salespointframework.core.DataInitializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.annotation.Order;
+import org.springframework.data.util.Pair;
+import org.springframework.stereotype.Component;
+
+import java.util.stream.Stream;
+
+@Component
+@Order(10)
+public class ServiceDataInitializer implements DataInitializer {
+
+	@Value("${orders.services.price}")
+	private int price;
+	@Value("${orders.services.currency}")
+	private String currency;
+
+	private Catalog<Service> catalog;
+
+	public ServiceDataInitializer(Catalog<Service> catalog) {
+		Assert.notNull(catalog, "catalog must not be null");
+
+		this.catalog = catalog;
+	}
+
+	@Override
+	public void initialize() {
+		if (!catalog.findByAllCategories().isEmpty()) {
+			return;
+		}
+
+		Stream.of(
+			Pair.of(new Service("kludge-heels", Money.of(price, currency)), ServiceCategory.KLUDGE.name()),
+			Pair.of(new Service("kludge-sole", Money.of(price, currency)), ServiceCategory.KLUDGE.name()),
+			Pair.of(new Service("kludge-seam", Money.of(price, currency)), ServiceCategory.KLUDGE.name()),
+			Pair.of(new Service("sewing-buttons", Money.of(price, currency)), ServiceCategory.SEWING.name()),
+			Pair.of(new Service("sewing-seam", Money.of(price, currency)), ServiceCategory.SEWING.name()),
+			Pair.of(new Service("sewing-patch", Money.of(price, currency)), ServiceCategory.SEWING.name()),
+			Pair.of(new Service("locksmith-copy", Money.of(price, currency)), ServiceCategory.LOCKSMITH.name()),
+			Pair.of(new Service("locksmith-engrave", Money.of(price, currency)), ServiceCategory.LOCKSMITH.name()),
+			Pair.of(new Service("cleaning-laundry", Money.of(price, currency)), ServiceCategory.CLEANING.name()),
+			Pair.of(new Service("cleaning-suits", Money.of(price, currency)), ServiceCategory.CLEANING.name()),
+			Pair.of(new Service("cleaning-leather", Money.of(price, currency)), ServiceCategory.CLEANING.name()),
+			Pair.of(new Service("electronics-replace", Money.of(price, currency)), ServiceCategory.ELECTRONICS.name()),
+			Pair.of(new Service("electronics-brazing", Money.of(price, currency)), ServiceCategory.ELECTRONICS.name()),
+			Pair.of(new Service("grindery-scissors", Money.of(price, currency)), ServiceCategory.GRINDERY.name()),
+			Pair.of(new Service("grindery-knifes", Money.of(price, currency)), ServiceCategory.GRINDERY.name())
+		).forEach(pair -> {
+			pair.getFirst().addCategory(pair.getSecond());
+			catalog.save(pair.getFirst());
+		});
+	}
+}
+
