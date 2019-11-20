ebbd474b14bb2f180031efbeb0b9eb51bf963740
Kien Dang Tran <Kien.Dang_Tran@mailbox.tu-dresden.de>
2019-11-20 12:27:43 +0100

    Add users

---
+++ src/main/java/missmint/users/model/Staff.java
@@ -0,0 +1,81 @@
+package missmint.users.model;
+
+import missmint.orders.service.ServiceCategory;
+import org.salespointframework.useraccount.UserAccount;
+import org.springframework.util.Assert;
+
+import javax.persistence.*;
+import java.util.Collections;
+import java.util.Set;
+
+@Entity
+public class Staff {
+
+	@Id
+	@GeneratedValue
+	private long id;
+
+	@OneToOne
+	private UserAccount userAccount;
+
+	private String lastName;
+	private String firstName;
+
+	@ElementCollection
+	private Set<ServiceCategory> skills = Collections.emptySet();
+
+	// empty constructor for Entity
+	public Staff() {}
+
+	public Staff(UserAccount userAccount, String firstName, String lastName) {
+
+		Assert.notNull(userAccount, "UserAccount cannot be null.");
+		Assert.hasText(lastName, "Name cannot be null or empty.");
+		Assert.hasText(firstName, "Name cannot be null or empty.");
+
+		this.userAccount = userAccount;
+		this.lastName = lastName;
+		this.firstName = firstName;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public UserAccount getUserAccount() {
+		return userAccount;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String lastName() {
+		return lastName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getUserName() {
+		return userAccount.getUsername();
+	}
+
+	public boolean addSkill(ServiceCategory category) {
+		return skills.add(category);
+	}
+
+	public Set<ServiceCategory> getSkills() {
+		return skills;
+	}
+
+	public Double calculateSalary() {
+		// TODO: implement salary calculation
+		return 0.0;
+	}
+}
