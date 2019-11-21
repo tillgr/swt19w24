347e6cbaa2b9efebb2a3704d407337eaa5bd7731
Till Große <till.grosse@web.de>
2019-11-21 15:48:00 +0100

    add raum hinzufügen

---
+++ src/main/java/missmint/rooms/AddRoomForm.java
@@ -0,0 +1,19 @@
+package missmint.rooms;
+
+import javax.validation.constraints.NotBlank;
+
+public class AddRoomForm {
+	private final @NotBlank(message="Darf nicht leer sein!") String name;
+
+	public AddRoomForm(String name){
+		this.name = name;
+	}
+
+	public String getName(){
+		return name;
+	}
+
+	public Room createRoom(EntriesRepository entries, RoomsRepository rooms){
+		return new Room(name, entries, rooms);
+	}
+}
