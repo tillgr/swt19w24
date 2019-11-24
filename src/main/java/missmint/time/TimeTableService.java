266061be9cf2ecda62a0fa5a736f7e5e5764c23c
vilaureu <vilaureu@users.noreply.github.com>
2019-11-24 08:53:21 +0100

    improve entry creation performance

--- src/main/java/missmint/time/TimeTableService.java
+++ src/main/java/missmint/time/TimeTableService.java
@@ -5,19 +5,20 @@ import missmint.orders.order.OrderService;
 import missmint.orders.service.MissMintService;
 import missmint.orders.service.ServiceCategory;
 import missmint.orders.service.ServiceService;
-import missmint.rooms.Room;
 import missmint.rooms.RoomRepository;
-import missmint.users.model.Staff;
 import missmint.users.repositories.StaffRepository;
 import org.salespointframework.time.BusinessTime;
 import org.springframework.data.util.Pair;
+import org.springframework.data.util.StreamUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 
 @Service
 public class TimeTableService {
@@ -50,32 +51,31 @@ public class TimeTableService {
 		ServiceCategory category = ServiceService.getCategory(service);
 		Assert.isTrue(orderService.isOrderAcceptable(service), "service must be acceptable");
 
-		LocalDate date = time.getTime().toLocalDate();
-		while (true) {
-			for (int slotIndex = 0; slotIndex < SLOTS.size(); slotIndex++) {
-				Pair<LocalTime, LocalTime> slot = SLOTS.get(slotIndex);
+		LocalDateTime now = time.getTime();
+		LongStream.range(0, Long.MAX_VALUE)
+			.mapToObj(now.toLocalDate()::plusDays)
+			.flatMap(date ->
+				IntStream.range(0, SLOTS.size())
+					.mapToObj(slotIndex -> Pair.of(slotIndex, SLOTS.get(slotIndex)))
+					.filter(indexedSlot -> now.isBefore(LocalDateTime.of(date, indexedSlot.getSecond().getFirst())))
+					.map(indexedSlot -> {
+						int slotIndex = indexedSlot.getFirst();
 
-				if (time.getTime().isAfter(LocalDateTime.of(date, slot.getSecond()))) {
-					continue;
-				}
-
-				for (Room room : rooms.findAll()) {
-					if (entries.existsByRoomAndDateAndSlot(room, date, slotIndex)) {
-						continue;
-					}
-
-					for (Staff staff : staffRepository.findAllBySkillsContaining(category)) {
-						if (entries.existsByStaffAndDateAndSlot(staff, date, slotIndex)) {
-							continue;
-						}
-
-						TimeTableEntry entry = new TimeTableEntry(order, staff, slotIndex, room, date);
-						entries.save(entry);
-						return;
-					}
-				}
+						return StreamUtils.createStreamFromIterator(rooms.findAll().iterator())
+							.filter(room -> !entries.existsByRoomAndDateAndSlot(room, date, slotIndex))
+							.findAny()
+							.flatMap(room ->
+								staffRepository.findAllBySkillsContaining(category)
+									.filter(staff -> !entries.existsByStaffAndDateAndSlot(staff, date, slotIndex))
+									.stream().findAny().map(staff -> new TimeTableEntry(order, staff, slotIndex, room, date))
+							);
+					})
+			)
+			.filter(Optional::isPresent)
+			.findFirst().ifPresent(entry -> {
+				//noinspection OptionalGetWithoutIsPresent
+				entries.save(entry.get());
 			}
-			date = date.plusDays(1);
-		}
+		);
 	}
 }
