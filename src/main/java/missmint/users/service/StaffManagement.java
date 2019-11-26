ed24d12474d2e48c4e22ab594b74db1faeca7558
vilaureu <vilaureu@users.noreply.github.com>
2019-11-26 11:45:37 +0100

    initialize a staff member with a skill

--- src/main/java/missmint/users/service/StaffManagement.java
+++ src/main/java/missmint/users/service/StaffManagement.java
@@ -71,6 +71,10 @@ public class StaffManagement {
 		return userAccountManager.findByUsername(userName);
 	}
 
+	public Optional<Staff> findStaffByUserName(String userName) {
+		return findByUserName("hans").flatMap(staffRepository::findByUserAccount);
+	}
+
 	public Optional<Staff> findStaffById(Long id) {
 		return staffRepository.findById(id);
 	}
