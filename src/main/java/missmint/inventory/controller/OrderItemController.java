a925566fb14a493c77d5479bad65e71ad87eba6e
Tobi <tobikoellner@gmail.com>
2019-12-02 20:01:22 +0100

    orderItem details added

--- src/main/java/missmint/inventory/controller/OrderItemController.java
+++ src/main/java/missmint/inventory/controller/OrderItemController.java
@@ -3,8 +3,12 @@ package missmint.inventory.controller;
 import missmint.inventory.forms.OrderItemForm;
 import missmint.inventory.manager.OrderItemManager;
 import missmint.inventory.products.OrderItem;
+import missmint.orders.order.MissMintOrder;
 import org.salespointframework.catalog.Catalog;
 import org.salespointframework.catalog.ProductIdentifier;
+import org.salespointframework.order.OrderManager;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,15 +22,19 @@ import javax.validation.Valid;
 public class OrderItemController {
 	private final OrderItemManager orderItemManager;
 	private final Catalog<OrderItem> OrderItemCatalog;
+	private final OrderManager<MissMintOrder> orderManager;
 
-	public OrderItemController(OrderItemManager orderItemManager, Catalog<OrderItem> orderItemCatalog) {
+	public OrderItemController(OrderItemManager orderItemManager, Catalog<OrderItem> orderItemCatalog, OrderManager<MissMintOrder> orderManager) {
 		this.orderItemManager = orderItemManager;
 		OrderItemCatalog = orderItemCatalog;
+		this.orderManager = orderManager;
 	}
 
 	@GetMapping("/orderItem")
 	@PreAuthorize("isAuthenticated()")
 	public String orderItem(Model model) {
+		Page<MissMintOrder> orders = orderManager.findAll(Pageable.unpaged());
+		model.addAttribute("orders", orders);
 		model.addAttribute("orderItem", OrderItemCatalog.findByCategory("ORDER_ITEM"));
 		return "orderItem";
 	}
