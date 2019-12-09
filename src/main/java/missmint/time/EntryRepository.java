94e0c61b5b39f120f115ed7f03874f5e1fc7150e
vilaureu <vilaureu@users.noreply.github.com>
2019-12-09 13:59:30 +0100

    fix table in table entries

--- src/main/java/missmint/time/EntryRepository.java
+++ src/main/java/missmint/time/EntryRepository.java
@@ -6,9 +6,11 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.util.Streamable;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 public interface EntryRepository extends CrudRepository<TimeTableEntry, Long> {
 	boolean existsByRoomAndDateAndSlot(Room room, LocalDate date, int slot);
 	boolean existsByStaffAndDateAndSlot(Staff staff, LocalDate date, int slot);
 	Streamable<TimeTableEntry> findAllByDateAfter(LocalDate date);
+	TimeTableEntry findByDateAndSlotAndRoom(LocalDate date, int slot, Room room);
 }
