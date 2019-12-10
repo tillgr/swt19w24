fdc7885a2357ee440ca4831f80bf4e257dcf1fcc
TheT87 <ytacc87@gmail.com>
2019-12-10 22:35:18 +0100

    test changed

--- src/test/java/missmint/inventory/controller/InventoryControllerTest.java
+++ src/test/java/missmint/inventory/controller/InventoryControllerTest.java
@@ -42,6 +42,7 @@ public class InventoryControllerTest {
 					.andExpect(model().attributeExists("material"));
 
 
+
 		}
 
 }
--- src/test/java/missmint/inventory/manager/MaterialManagerTest.java
+++ src/test/java/missmint/inventory/manager/MaterialManagerTest.java
@@ -1,25 +1,20 @@
 package missmint.inventory.manager;
 
 import missmint.inventory.products.Material;
+
 import org.javamoney.moneta.Money;
-import org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.salespointframework.catalog.Catalog;
-import org.salespointframework.inventory.InventoryItemIdentifier;
 import org.salespointframework.inventory.UniqueInventory;
 import org.salespointframework.inventory.UniqueInventoryItem;
 import org.salespointframework.quantity.Metric;
 import org.salespointframework.quantity.Quantity;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.util.Pair;
 import org.springframework.data.util.Streamable;
-import org.springframework.test.context.event.annotation.BeforeTestExecution;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.Assert;
+
 
 import java.util.Objects;
 import java.util.Optional;
@@ -28,73 +23,71 @@ import java.util.stream.Stream;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.salespointframework.core.Currencies.EURO;
 
+@SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
 @SpringBootTest
-@AutoConfigureMockMvc
-@Transactional
 public class MaterialManagerTest {
 
 	@Autowired
-	private Catalog<Material> materialTestCatalog;
+	private Catalog<Material> materialCatalog;
 	@Autowired
-	private UniqueInventory<UniqueInventoryItem> materialTestInventory;
+	private UniqueInventory<UniqueInventoryItem> materialInventory;
 	@Autowired
-	private MaterialManager materialTestManager;
+	private MaterialManager materialManager;
 
-	@BeforeTestExecution
-	public void setUp(){
+	@BeforeEach
+	public void setUp() {
 		Stream.of(
 				Pair.of(new Material("uTest", Money.of(0.5, EURO), Metric.UNIT), "UNIT_MATERIAL"),
-				Pair.of(new Material("mTest", Money.of(0.02,EURO), Metric.METER), "METER_MATERIAL"),
-				Pair.of(new Material("sqTest", Money.of(0.02,EURO), Metric.SQUARE_METER), "SQUARE_METER_MATERIAL"),
-				Pair.of(new Material("lTest", Money.of(1,EURO), Metric.LITER), "LITER_MATERIAL")
-		).forEach(materialStringPair-> {
+				Pair.of(new Material("mTest", Money.of(0.02, EURO), Metric.METER), "METER_MATERIAL"),
+				Pair.of(new Material("sqTest", Money.of(0.02, EURO), Metric.SQUARE_METER), "SQUARE_METER_MATERIAL"),
+				Pair.of(new Material("lTest", Money.of(1, EURO), Metric.LITER), "LITER_MATERIAL")
+		).forEach(materialStringPair -> {
 			materialStringPair.getFirst().addCategory(materialStringPair.getSecond());
-			materialTestCatalog.save(materialStringPair.getFirst());
+			materialCatalog.save(materialStringPair.getFirst());
 		});
-			materialTestCatalog.findByCategory("UNIT_MATERIAL").forEach(material -> {
-				if (materialTestInventory.findByProduct(material).isEmpty()) {
-					materialTestInventory.save(new UniqueInventoryItem(material, Quantity.of(100, Metric.UNIT)));
-				}
-			});
-			materialTestCatalog.findByCategory("SQUARE_METER_MATERIAL").forEach(material -> {
-				if (materialTestInventory.findByProduct(material).isEmpty()) {
-					materialTestInventory.save(new UniqueInventoryItem(material, Quantity.of(100, Metric.SQUARE_METER)));
-				}
-			});
-			materialTestCatalog.findByCategory("METER_MATERIAL").forEach(material -> {
-				if (materialTestInventory.findByProduct(material).isEmpty()) {
-					materialTestInventory.save(new UniqueInventoryItem(material, Quantity.of(100, Metric.METER)));
-				}
-			});
-			materialTestCatalog.findByCategory("LITER_MATERIAL").forEach(material -> {
-				if (materialTestInventory.findByProduct(material).isEmpty()) {
-					materialTestInventory.save(new UniqueInventoryItem(material, Quantity.of(100, Metric.LITER)));
-				}
-			});
-		}
-
-		@Test
-		public void consumeTest() {
-			setUp();
-			Streamable<Material> uMaterial = materialTestCatalog.findByName("uTest");
-			Optional<UniqueInventoryItem> i1 = materialTestInventory.findByProductIdentifier(Objects.requireNonNull(uMaterial.toList().get(0).getId()));
-			i1.ifPresent(test -> {
-				int i = materialTestManager.manualConsume(test,20);
-				assertThat(test.getQuantity().getAmount().intValueExact() - i).isEqualTo(80);
-			});
-		}
-
-		@Test
-		public void consumeAutoRestockTest(){
-		setUp();
-			Streamable<Material> material = materialTestCatalog.findByName("uTest");
-			System.out.println(material.toList());
-			Optional<UniqueInventoryItem> i1 = materialTestInventory.findByProductIdentifier(Objects.requireNonNull(material.toList().get(0).getId()));
-			i1.ifPresent(test2 -> {
-				int i2 = materialTestManager.manualConsume(test2,110);
-				assertThat(test2.getQuantity().getAmount().intValueExact() - i2).isEqualTo(0);
-			});
-		}
-
+		materialCatalog.findByCategory("UNIT_MATERIAL").forEach(material -> {
+			if (materialInventory.findByProduct(material).isEmpty()) {
+				materialInventory.save(new UniqueInventoryItem(material, Quantity.of(100, Metric.UNIT)));
+			}
+		});
+		materialCatalog.findByCategory("SQUARE_METER_MATERIAL").forEach(material -> {
+			if (materialInventory.findByProduct(material).isEmpty()) {
+				materialInventory.save(new UniqueInventoryItem(material, Quantity.of(100, Metric.SQUARE_METER)));
+			}
+		});
+		materialCatalog.findByCategory("METER_MATERIAL").forEach(material -> {
+			if (materialInventory.findByProduct(material).isEmpty()) {
+				materialInventory.save(new UniqueInventoryItem(material, Quantity.of(100, Metric.METER)));
+			}
+		});
+		materialCatalog.findByCategory("LITER_MATERIAL").forEach(material -> {
+			if (materialInventory.findByProduct(material).isEmpty()) {
+				materialInventory.save(new UniqueInventoryItem(material, Quantity.of(100, Metric.LITER)));
+			}
+		});
 	}
+
+	@Test
+	public void consumeMaterialWithoutRestockTest() {
+
+		Streamable<Material> sqMaterialStreamable = materialCatalog.findByName("sqTest");
+		Optional<UniqueInventoryItem> sqMaterial = materialInventory.findByProductIdentifier(Objects.requireNonNull(sqMaterialStreamable.toList().get(0).getId()));
+
+		sqMaterial.ifPresent(it -> {
+			UniqueInventoryItem item = materialManager.checkAndConsume(it.getId(),20);
+			assertThat(item.getQuantity().getAmount().intValueExact()).isEqualTo(80);
+		});
+	}
+
+	@Test
+	public void consumeMaterialWithRestock(){
+		Streamable<Material> uMaterialStreamable = materialCatalog.findByName("sqTest");
+		Optional<UniqueInventoryItem> uMaterial = materialInventory.findByProductIdentifier(Objects.requireNonNull(uMaterialStreamable.toList().get(0).getId()));
+
+		uMaterial.ifPresent(material -> {
+			UniqueInventoryItem item = materialManager.checkAndConsume(material.getId(),1000);
+			assertThat(item.getQuantity().getAmount().intValueExact()).isEqualTo(20);
+		});
+	}
+}
 
