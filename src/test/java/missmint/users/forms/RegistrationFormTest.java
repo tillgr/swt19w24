1795c2fa1446ce973f3f444f70ed974b8a830567
Kien Dang Tran <Kien.Dang_Tran@mailbox.tu-dresden.de>
2019-12-12 19:38:36 +0100

    Add test for registration form

---
+++ src/test/java/missmint/users/forms/RegistrationFormTest.java
@@ -0,0 +1,55 @@
+package missmint.users.forms;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import javax.validation.Validation;
+import javax.validation.Validator;
+import java.math.BigDecimal;
+
+public class RegistrationFormTest {
+
+	private Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+
+	@Test
+	void validForm() {
+		var form = new RegistrationForm("Jacob", "Smith", "XXX", "1234", BigDecimal.valueOf(10));
+		var violations = validator.validate(form);
+		Assertions.assertTrue(violations.isEmpty());
+	}
+
+	@Test
+	void blankFirstName() {
+		var form = new RegistrationForm(" ", "Smith", "XXX", "1234", BigDecimal.valueOf(10));
+		var violations = validator.validate(form);
+		Assertions.assertEquals(1, violations.size());
+	}
+
+	@Test
+	void blankLastName() {
+		var form = new RegistrationForm("Jacob", " ", "XXX", "1234", BigDecimal.valueOf(10));
+		var violations = validator.validate(form);
+		Assertions.assertEquals(1, violations.size());
+	}
+
+	@Test
+	void blankUsername() {
+		var form = new RegistrationForm("Jacob", "Smith", "   ", "1234", BigDecimal.valueOf(10));
+		var violations = validator.validate(form);
+		Assertions.assertEquals(1, violations.size());
+	}
+
+	@Test
+	void emptyPassword() {
+		var form = new RegistrationForm("Jacob", "Smith", "xxx", "", BigDecimal.valueOf(10));
+		var violations = validator.validate(form);
+		Assertions.assertEquals(1, violations.size());
+	}
+
+	@Test
+	void negativeSalary() {
+		var form = new RegistrationForm("Jacob", "Smith", "xxx", "12", BigDecimal.valueOf(-10));
+		var violations = validator.validate(form);
+		Assertions.assertEquals(1, violations.size());
+	}
+}
