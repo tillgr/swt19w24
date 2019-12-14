7f4f9b15e06f7033a91dffe7429190c59d8851fb
Till Große <till.grosse@web.de>
2019-12-14 14:46:54 +0100

    adress comments

--- src/test/java/missmint/rooms/RoomTest.java
+++ src/test/java/missmint/rooms/RoomTest.java
@@ -1,6 +1,7 @@
 package missmint.rooms;
 
 import missmint.inventory.products.OrderItem;
+import missmint.orders.order.MissMintOrder;
 import missmint.orders.service.MissMintService;
 import missmint.orders.service.ServiceCategory;
 import missmint.rooms.Room;
@@ -26,13 +27,31 @@ import org.springframework.transaction.annotation.Transactional;
 import java.math.BigDecimal;
 import java.time.Duration;
 import java.time.LocalDate;
+import java.util.Locale;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
 @SpringBootTest
 public class RoomTest {
-	//TODO
+
+	@Test
+	@Autowired
+	void canNotPickUp() throws Exception {
+		MissMintOrder order = createOrder();
+		orderManager.save(order);
+
+		mvc.perform(get(String.format("/orders/pickup/%s", order.getId())).locale(Locale.ROOT))
+				.andExpect(status().isBadRequest());
+
+		mvc.perform(post(String.format("/orders/pickup/%s", order.getId())).locale(Locale.ROOT).with(csrf()))
+				.andExpect(status().isBadRequest());
+	}
+
 }
