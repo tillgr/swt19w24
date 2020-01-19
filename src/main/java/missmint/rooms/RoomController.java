f5ecd5ce5ae6dbfb8d30f77909e5d7ea82cffaa1
vilaureu <vilaureu@users.noreply.github.com>
2020-01-20 00:25:20 +0100

    fix room controller

--- src/main/java/missmint/rooms/RoomController.java
+++ src/main/java/missmint/rooms/RoomController.java
@@ -62,7 +62,7 @@ public class RoomController {
 	 * @return redirect to the rooms page
 	 */
 	@PostMapping("/rooms/add")
-	public String addRoom(Model model, @Valid @ModelAttribute("form") AddRoomForm form, Authentication auth, Errors errors) {
+	public String addRoom(Model model, @Valid @ModelAttribute("form") AddRoomForm form, Errors errors, Authentication auth) {
 		if (errors.hasErrors()) {
 			return showRooms(model, form, auth);
 		}
